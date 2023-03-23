c69627b91f79803cd87d564b337c295417103713
eloise26malam <rse21qju@uea.ac.uk>
2023-03-23 12:52:28 +0000

    cleaning names

--- Scripts/analysis.R
+++ Scripts/analysis.R
@@ -5,4 +5,15 @@ library(ggplot2)
 library(colorBlindness)
 ## Import data ----
 cricket_original <- read_csv("Data/cricket_song.csv")
-head (cricket_original) #check data loaded sucessfully
+head (cricket_original) #check data loaded successfully
+
+#DATA WRANGLING----
+##Clean ----
+cricket_clean <- janitor::clean_names(cricket_original) #snakecase
+colnames(cricket_clean)
+cricket_clean <- rename(cricket_clean, "size_mm"="pronotum",
+                        "start_mass"="mass0",
+                        "song_duration"="song_week1",
+                        "weight_change"="delta_smi")
+#names easier to work with 
+#better descriptions of variables
