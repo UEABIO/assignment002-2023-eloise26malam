63b1568d23fc1644cb8bd3ceb962662a819efa6e
eloise26malam <rse21qju@uea.ac.uk>
2023-04-10 22:02:23 +0000

    patchwork of emmeans and diet/weight graphs saved

--- Scripts/analysis.R
+++ Scripts/analysis.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(ggplot2)
 library(colorBlindness)
+library(patchwork)
 ## Import data 📥 ----
 cricket_original <- read_csv("Data/cricket_song.csv")
 head (cricket_original) #check data loaded successfully
@@ -29,71 +30,6 @@ cricket_abs #removes neg values
 
 
 
-#PLOTS 📊 ----
-## size, start mass----
-cricket_abs %>% ggplot(aes(x=size_mm, y=start_mass))+
-  geom_point()+ geom_smooth(method="lm",    
-                           se=FALSE)
-
-## diet, weight change ----
-diet_weightchange <- cricket_abs %>% filter(song_duration !=0) %>%
-  ggplot(aes(x=diet, y=weight_change, group=diet))+
-  theme_classic()+
-  geom_rect(xmin= -Inf,
-            xmax= Inf,
-            ymin= -Inf, 
-            ymax=0, fill="#ff4f4f")+
-  geom_rect(xmin= -Inf,
-            xmax= Inf,
-            ymin= 0, 
-            ymax= Inf, fill="#6bff72")+
-  geom_boxplot(aes(fill=diet), show.legend = FALSE, outlier.size=0.5) +
-  scale_fill_gradient(low="#616161", high="#fcfcfc")+
-  scale_x_continuous(name="Diet (nutritional %)", 
-                     breaks=seq(12,84,12))+
-  scale_y_continuous(name= "Weight Change (g)",
-                     breaks=seq(-0.060, 0.090, 0.03))
-diet_weightchange
-ggsave("Graphs/diet_weightchange_march23.png", width=14, height=7.5, units="cm", dpi=300)
-colorBlindness::cvdPlot() #colours are accessible
-##diet, duration----
-diet_duration <- cricket_abs %>% filter(song_duration !=0) %>% 
-  ggplot(aes(x=diet, y=song_duration, group=diet))+
-  geom_boxplot(aes(fill=diet), show.legend = FALSE, outlier.size=0.5) +
-  scale_fill_gradient(low="#616161", high="#fcfcfc")+
-  scale_x_continuous(name="Diet (nutritional %)", 
-                     breaks=seq(12,84,12))+
-  scale_y_continuous(name= "Song Duration (minutes)")+
-  theme_classic()
-diet_duration 
-ggsave("Graphs/diet_duration_march23.png", width=14, height=7.5, units="cm", dpi=300)
-##high, med, low diet, song----
-high_diet <- cricket_abs %>% filter(diet>=60, song_duration != 0) %>% 
-  ggplot(aes(x=song_duration, y=weight_change))+
-  geom_point() +  geom_smooth(method="lm",    
-                              se=FALSE)
-high_diet  
-
-mid_diet <- cricket_abs %>% filter(diet==48, song_duration != 0) %>% 
-  ggplot(aes(x=song_duration, y=weight_change))+
-  geom_point() + geom_smooth(method="lm",    
-                             se=FALSE)
-mid_diet
-
-low_diet <- cricket_abs %>% filter(diet<=36, song_duration != 0) %>% 
-  ggplot(aes(x=song_duration, y=weight_change))+
-  geom_point()+ geom_smooth(method="lm",    
-                            se=FALSE)
-low_diet
-
-##weight change, duration----
-cricket_abs %>% filter(song_duration !=0) %>%
-  ggplot(aes(x=song_duration, y=weight_change, colour=diet))+
-  geom_point()+geom_smooth(method="lm", colour="BLACK",   
-                           se=FALSE)+
-  scale_colour_gradient(low="#2b0002", high="#ff2930")+
-  theme_classic()
-colorBlindness::cvdPlot() #colours are accessible
 
 #MODEL----
 lsmodel1 <- lm(song_duration ~ diet, data=cricket_abs)
@@ -117,19 +53,79 @@ checklsm_cat <- performance::check_model(lsmodel_cat)
 checklsm_cat
 #t value = 
 
-##Emmeans graphs----
-###Diet, weight 
-means_dw <- emmeans::emmeans(lsmodel_dw, specs = ~ diet_category)
+
+#PLOTS 📊 ----
 group_colour <-c("#d90b15", "#f79011", "#05f52d")
+
+##Emmeans graphs----
+###Diet, weight -----
+means_dw <- emmeans::emmeans(lsmodel_dw, specs = ~ diet_category)
 means_dw
-means_dw %>% 
+
+plot_means_dw<-means_dw %>% 
   as_tibble() %>% 
   ggplot(aes(x=diet_category, 
              y=emmean, colour=diet_category))+
   scale_colour_manual(values = group_colour)+
   geom_pointrange(aes(
     ymin=lower.CL, 
-    ymax=upper.CL))+
+    ymax=upper.CL), show.legend = FALSE)+
+  labs(x="Diet Category", y="Mean Weight Change (g)")+
+  scale_y_continuous(position="right")+
   theme_classic()
 
-              
+
+## size, start mass----
+cricket_abs %>% ggplot(aes(x=size_mm, y=start_mass))+
+  geom_point()+ geom_smooth(method="lm",    
+                            se=FALSE)
+
+## diet, weight change ----
+diet_weightchange <- cricket_abs %>% filter(song_duration !=0) %>%
+  ggplot(aes(x=diet, y=weight_change, group=diet))+
+  theme_classic()+
+  geom_rect(xmin= -Inf,
+            xmax= Inf,
+            ymin= -Inf, 
+            ymax=0, fill="#ff4f4f")+
+  geom_rect(xmin= -Inf,
+            xmax= Inf,
+            ymin= 0, 
+            ymax= Inf, fill="#6bff72")+
+  geom_boxplot(aes(fill=diet), show.legend = FALSE, outlier.size=0.5) +
+  scale_fill_gradient(low="#616161", high="#fcfcfc")+
+  scale_x_continuous(name="Diet (nutritional %)", 
+                     breaks=seq(12,84,12))+
+  scale_y_continuous(name= "Weight Change (g)",
+                     breaks=seq(-0.060, 0.090, 0.03))
+diet_weightchange
+ggsave("Graphs/diet_weightchange_march23.png", width=14, height=7.5, units="cm", dpi=300)
+colorBlindness::cvdPlot() #colours are accessible
+
+
+##diet, duration----
+diet_duration <- cricket_abs %>% filter(song_duration !=0) %>% 
+  ggplot(aes(x=diet, y=song_duration, group=diet))+
+  geom_boxplot(aes(fill=diet), show.legend = FALSE, outlier.size=0.5) +
+  scale_fill_gradient(low="#616161", high="#fcfcfc")+
+  scale_x_continuous(name="Diet (nutritional %)", 
+                     breaks=seq(12,84,12))+
+  scale_y_continuous(name= "Song Duration (minutes)")+
+  theme_classic()
+diet_duration 
+ggsave("Graphs/diet_duration_march23.png", width=14, height=7.5, units="cm", dpi=300)
+
+
+##weight change, duration----
+cricket_abs %>% filter(song_duration !=0) %>%
+  ggplot(aes(x=song_duration, y=weight_change, colour=diet))+
+  geom_point()+geom_smooth(method="lm", colour="BLACK",   
+                           se=FALSE)+
+  scale_colour_gradient(low="#2b0002", high="#05f52d")+
+  theme_classic()
+colorBlindness::cvdPlot() #colours are accessible
+
+#PATCHWORK 🧶----
+
+diet_weightchange + plot_means_dw + plot_layout(guides = "collect", widths = c(2, 1))
+ggsave("Graphs/dw_means_april23.png", width=14, height=7.5, units="cm", dpi=300)
